c0fa7d1cdaf7b5de968966b39c6a453ce4b72342
sathyapriya <spriya2306@gmail.com>
2019-11-09 23:39:05 +0530

    [SathyaPriya]Add.Test case to check the direction not equal to the right direction, when rotated left

--- src/test/java/com/tw/vapasi/assigments/RoverTest.java
+++ src/test/java/com/tw/vapasi/assigments/RoverTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class RoverTest {
 
@@ -17,4 +18,13 @@ public class RoverTest {
         assertEquals(Direction.WEST, rover.processInstruction("L"));
     }
 
+    @Test
+    @DisplayName("Should the direction not be SOUTH, when the direction is EAST and instruction to turn is left ")
+    void shouldTheDirectionNotBeSouthWhenEastRotateLeft() {
+        Plateau plateau = new Plateau(5, 5);
+        Coordinates startingCoordinates = new Coordinates(3, 4);
+        Rover rover = new Rover(plateau, startingCoordinates, Direction.EAST);
+
+        assertNotEquals(Direction.SOUTH, rover.processInstruction("L"));
+    }
 }
