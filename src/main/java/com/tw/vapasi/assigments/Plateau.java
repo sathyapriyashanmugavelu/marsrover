845ab84aee868056225d595426cb39a23b7209cc
sathyapriya <spriya2306@gmail.com>
2019-11-09 20:17:17 +0530

    [SathyaPriya]Add.Check the coordinates are out of bound when x value is out of bound

--- src/main/java/com/tw/vapasi/assigments/Plateau.java
+++ src/main/java/com/tw/vapasi/assigments/Plateau.java
@@ -10,13 +10,10 @@ public class Plateau {
     }
 
     public boolean withInBounds(Plateau plateau){
-        if(plateau.xCoordinate > 0 && plateau.xCoordinate <= this.xCoordinate){
-            if(plateau.yCoordinate > 0 && plateau.yCoordinate <= this.yCoordinate){
+        if(plateau.xCoordinate <= this.xCoordinate){
                 return true;
             }
             return false;
-        }
-        return false;
     }
 
 }
--- src/test/java/com/tw/vapasi/assigments/PlateauTest.java
+++ src/test/java/com/tw/vapasi/assigments/PlateauTest.java
@@ -1,5 +1,6 @@
 package com.tw.vapasi.assigments;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,10 +8,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlateauTest {
     @Test
+    @DisplayName("Should the coordinates be with in the bounds when lower-left coordinates are (3,4) and upper right coordinates are (5,5) ")
     void shouldTheCoordinatesBeWithInTheBounds () {
         Plateau initializeCoordinates = new Plateau(5, 5);
         Plateau givenCoordinates = new Plateau(3, 4);
         assertEquals(true,initializeCoordinates.withInBounds(givenCoordinates));
+    }
+
+    @Test
+    @DisplayName("Should the coordinates be out of the bounds when lower-left coordinates are (6,5) and upper right coordinates are (5,5) ")
+    void shouldTheCoordinatesBeOutOfBoundsWhenXCoordinateIsOutOfBound () {
+        Plateau initializeCoordinates = new Plateau(5, 5);
+        Plateau givenCoordinates = new Plateau(6, 5);
+        assertEquals(false,initializeCoordinates.withInBounds(givenCoordinates));
     }
 }
 
