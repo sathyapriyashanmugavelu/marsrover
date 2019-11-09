faf3a815b33529ad0a44357cb52b8e65b83cf7ad
sathyapriya <spriya2306@gmail.com>
2019-11-09 20:36:35 +0530

    [SathyaPriya]Refactor.Reformat and Renaming of variables

--- src/main/java/com/tw/vapasi/assigments/Plateau.java
+++ src/main/java/com/tw/vapasi/assigments/Plateau.java
@@ -4,18 +4,17 @@ public class Plateau {
     private int xCoordinate;
     private int yCoordinate;
 
-    Plateau(int xCoordinate,int yCoordinate){
-        this.xCoordinate=xCoordinate;
-        this.yCoordinate=yCoordinate;
+    Plateau(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
     }
 
-    public boolean withInBounds(Plateau plateau){
-        if(plateau.xCoordinate <= this.xCoordinate && plateau.xCoordinate >= 0) {
-            if(plateau.yCoordinate <= this.yCoordinate && plateau.yCoordinate >=0) {
+    public boolean withInBounds(Plateau plateau) {
+        if (plateau.xCoordinate <= this.xCoordinate && plateau.xCoordinate >= 0) {
+            if (plateau.yCoordinate <= this.yCoordinate && plateau.yCoordinate >= 0) {
                 return true;
             }
         }
-            return false;
+        return false;
     }
-
 }
--- src/test/java/com/tw/vapasi/assigments/PlateauTest.java
+++ src/test/java/com/tw/vapasi/assigments/PlateauTest.java
@@ -9,62 +9,59 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PlateauTest {
     @Test
     @DisplayName("Should the coordinates be with in the bounds when lower-left coordinates are (3,4) and upper right coordinates are (5,5) ")
-    void shouldTheCoordinatesBeWithInTheBounds () {
-        Plateau initializeCoordinates = new Plateau(5, 5);
-        Plateau givenCoordinates = new Plateau(3, 4);
-        assertEquals(true,initializeCoordinates.withInBounds(givenCoordinates));
+    void shouldTheCoordinatesBeWithInTheBounds() {
+        Plateau upperRightCoordinates = new Plateau(5, 5);
+        Plateau lowerLeftCoordinates = new Plateau(3, 4);
+        assertEquals(true, upperRightCoordinates.withInBounds(lowerLeftCoordinates));
     }
 
     @Test
     @DisplayName("Should the coordinates be out of the bounds when lower-left coordinates are (6,5) and upper right coordinates are (5,5) ")
-    void shouldTheCoordinatesBeOutOfBoundsWhenXCoordinateIsOutOfBound () {
+    void shouldTheCoordinatesBeOutOfBoundsWhenXCoordinateIsOutOfBound() {
         Plateau initializeCoordinates = new Plateau(5, 5);
         Plateau givenCoordinates = new Plateau(6, 5);
-        assertEquals(false,initializeCoordinates.withInBounds(givenCoordinates));
+        assertEquals(false, initializeCoordinates.withInBounds(givenCoordinates));
     }
 
     @Test
     @DisplayName("Should the coordinates be out of the bounds when x coordinate is negative")
-    void shouldTheCoordinatesBeOutOfBoundsWhenXCoordinateIsNegative () {
+    void shouldTheCoordinatesBeOutOfBoundsWhenXCoordinateIsNegative() {
         Plateau initializeCoordinates = new Plateau(5, 5);
         Plateau givenCoordinates = new Plateau(-2, 4);
-        assertEquals(false,initializeCoordinates.withInBounds(givenCoordinates));
+        assertEquals(false, initializeCoordinates.withInBounds(givenCoordinates));
     }
-
 
     @Test
     @DisplayName("Should the coordinates be out of the bounds when lower-left coordinates are (4,6) and upper right coordinates are (5,5) ")
-    void shouldTheCoordinatesBeOutOfBoundsWhenYCoordinateIsOutOfBound () {
+    void shouldTheCoordinatesBeOutOfBoundsWhenYCoordinateIsOutOfBound() {
         Plateau initializeCoordinates = new Plateau(5, 5);
         Plateau givenCoordinates = new Plateau(4, 6);
-        assertEquals(false,initializeCoordinates.withInBounds(givenCoordinates));
+        assertEquals(false, initializeCoordinates.withInBounds(givenCoordinates));
     }
 
     @Test
     @DisplayName("Should the coordinates be out of the bounds when y coordinate is negative")
-    void shouldTheCoordinatesBeOutOfBoundsWhenYCoordinateIsNegative () {
+    void shouldTheCoordinatesBeOutOfBoundsWhenYCoordinateIsNegative() {
         Plateau initializeCoordinates = new Plateau(5, 5);
         Plateau givenCoordinates = new Plateau(4, -4);
-        assertEquals(false,initializeCoordinates.withInBounds(givenCoordinates));
+        assertEquals(false, initializeCoordinates.withInBounds(givenCoordinates));
     }
 
     @Test
     @DisplayName("Should the coordinates be out of the bounds when x and y coordinates are negative")
-    void shouldTheCoordinatesBeOutOfBoundsWhenXAndYCoordinateAreNegative () {
+    void shouldTheCoordinatesBeOutOfBoundsWhenXAndYCoordinateAreNegative() {
         Plateau initializeCoordinates = new Plateau(5, 5);
         Plateau givenCoordinates = new Plateau(-4, -4);
-        assertEquals(false,initializeCoordinates.withInBounds(givenCoordinates));
+        assertEquals(false, initializeCoordinates.withInBounds(givenCoordinates));
     }
 
     @Test
     @DisplayName("Should the coordinates be out of the bounds when x and y coordinates are out of bound")
-    void shouldTheCoordinatesBeOutOfBoundsWhenXAndYCoordinateAreOutOfBound () {
+    void shouldTheCoordinatesBeOutOfBoundsWhenXAndYCoordinateAreOutOfBound() {
         Plateau initializeCoordinates = new Plateau(5, 5);
         Plateau givenCoordinates = new Plateau(7, 9);
-        assertEquals(false,initializeCoordinates.withInBounds(givenCoordinates));
+        assertEquals(false, initializeCoordinates.withInBounds(givenCoordinates));
     }
-
-
-    }
+}
 
 
